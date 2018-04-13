7adaafb01d3a2a8bc0549cc53628d5cbfc43481d
Mickael Jeanroy <mickael.jeanroy@gmail.com>
2018-04-13 11:36:43 +0200

    chore: rename class and packages with a more conventional name

---
+++ rest-assert-core/src/test/java/com/github/mjeanroy/restassert/internal/assertions/http/headers/headerequalto/IsFrameOptionsEqualToTest.java
@@ -0,0 +1,87 @@
+/**
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2014-2016 <mickael.jeanroy@gmail.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package com.github.mjeanroy.restassert.internal.assertions.http.headers.headerequalto;
+
+import com.github.mjeanroy.restassert.internal.assertions.AssertionResult;
+import com.github.mjeanroy.restassert.data.FrameOptions;
+import com.github.mjeanroy.restassert.internal.data.HttpResponse;
+import com.github.mjeanroy.restassert.tests.mocks.HttpResponseMockBuilderImpl;
+import com.github.mjeanroy.restassert.tests.models.Header;
+import org.junit.Test;
+
+import static com.github.mjeanroy.restassert.tests.models.Header.header;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class IsFrameOptionsEqualToTest extends AbstractHttpHeaderEqualToTest {
+
+	@Override
+	protected Header getHeader() {
+		return header(getName(), getValue().value());
+	}
+
+	@Override
+	protected AssertionResult invoke(HttpResponse response) {
+		return assertions.isFrameOptionsEqualTo(response, getValue());
+	}
+
+	@Override
+	protected boolean allowMultipleValues() {
+		return true;
+	}
+
+	@Test
+	public void it_should_check_that_allow_from_value_match() {
+		HttpResponse httpResponse = new HttpResponseMockBuilderImpl()
+			.addHeader(getName(), "allow-from https://www.google.com")
+			.build();
+
+		AssertionResult r = assertions.isFrameOptionsEqualTo(httpResponse, FrameOptions.ALLOW_FROM);
+
+		assertThat(r).isNotNull();
+		assertThat(r.isSuccess()).isTrue();
+		assertThat(r.isFailure()).isFalse();
+	}
+
+	@Test
+	public void it_should_check_that_allow_from_value_does_not_match() {
+		HttpResponse httpResponse = new HttpResponseMockBuilderImpl()
+			.addHeader(getName(), "deny")
+			.build();
+
+		AssertionResult r = assertions.isFrameOptionsEqualTo(httpResponse, FrameOptions.ALLOW_FROM);
+
+		assertThat(r).isNotNull();
+		assertThat(r.isSuccess()).isFalse();
+		assertThat(r.isFailure()).isTrue();
+	}
+
+	private String getName() {
+		return "X-Frame-Options";
+	}
+
+	private FrameOptions getValue() {
+		return FrameOptions.SAME_ORIGIN;
+	}
+}
---
+++ rest-assert-core/src/test/java/com/github/mjeanroy/restassert/internal/assertions/http/mimetype/AbstractMimeTypeInTest.java
@@ -0,0 +1,104 @@
+/**
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2014-2016 <mickael.jeanroy@gmail.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package com.github.mjeanroy.restassert.internal.assertions.http.mimetype;
+
+import com.github.mjeanroy.restassert.error.http.ShouldHaveMimeType;
+import com.github.mjeanroy.restassert.internal.assertions.AbstractAssertionsTest;
+import com.github.mjeanroy.restassert.internal.assertions.AssertionResult;
+import com.github.mjeanroy.restassert.internal.assertions.HttpResponseAssertions;
+import com.github.mjeanroy.restassert.internal.data.HttpResponse;
+import com.github.mjeanroy.restassert.tests.mocks.HttpResponseMockBuilderImpl;
+import com.github.mjeanroy.restassert.tests.models.Header;
+import com.github.mjeanroy.restassert.utils.Mapper;
+import com.github.mjeanroy.restassert.utils.Utils;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.List;
+
+import static com.github.mjeanroy.restassert.tests.models.Header.header;
+
+public abstract class AbstractMimeTypeInTest extends AbstractAssertionsTest<HttpResponse> {
+
+	HttpResponseAssertions assertions;
+
+	@Before
+	public void setUp() {
+		assertions = HttpResponseAssertions.instance();
+	}
+
+	@Test
+	public void it_should_pass_with_expected_mime_type() {
+		List<Header> headers = getHeaders();
+		for (Header header : headers) {
+			AssertionResult result = invoke(newResponse(header));
+			checkSuccess(result);
+		}
+	}
+
+	@Test
+	public void it_should_fail_with_if_response_is_not_expected_mime_type() {
+		final List<Header> headers = getHeaders();
+		final List<String> mimeType = getMimeTypes();
+
+		int i = 0;
+		for (Header h : headers) {
+			final String expectedMimeType = mimeType.get(i);
+			i++;
+
+			final String actualMimeType = expectedMimeType + "foo";
+
+			final String expectedName = h.getName();
+			final String expectedValue = h.getValue();
+			final String actualValue = expectedValue.replace(expectedMimeType, actualMimeType);
+			final Header header = header(expectedName, actualValue);
+
+			AssertionResult result = invoke(newResponse(header));
+
+			checkError(result,
+					ShouldHaveMimeType.class,
+					"Expecting response to have mime type in %s but was %s",
+					mimeType, actualMimeType
+			);
+		}
+	}
+
+	private HttpResponse newResponse(Header header) {
+		return new HttpResponseMockBuilderImpl()
+			.addHeader(header)
+			.build();
+	}
+
+	protected abstract List<String> getMimeTypes();
+
+	private List<Header> getHeaders() {
+		return Utils.map(getMimeTypes(), new Mapper<String, Header>() {
+			@Override
+			public Header apply(String input) {
+				return header("Content-Type", input + ";charset=UTF-8");
+			}
+		});
+	}
+}
