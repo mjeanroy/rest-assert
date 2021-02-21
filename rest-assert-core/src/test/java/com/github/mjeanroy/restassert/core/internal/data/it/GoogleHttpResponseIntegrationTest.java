6bc7a496f791067a6804b66a5736a1929480fc81
Mickael Jeanroy <mickael.jeanroy@gmail.com>
2021-02-21 21:23:37 +0100

    chore: update copyright header

--- rest-assert-core/src/test/java/com/github/mjeanroy/restassert/core/internal/data/it/GoogleHttpResponseIntegrationTest.java
+++ rest-assert-core/src/test/java/com/github/mjeanroy/restassert/core/internal/data/it/GoogleHttpResponseIntegrationTest.java
@@ -1,7 +1,7 @@
 /**
  * The MIT License (MIT)
  * <p>
- * Copyright (c) 2014-2018 Mickael Jeanroy
+ * Copyright (c) 2014-2021 Mickael Jeanroy
  * <p>
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
